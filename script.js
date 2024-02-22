ebc27d51198c3d297abc9479cd4917643f0b2fbd
Kayke Araujo Guimarães <90934419+K4Z1NH0@users.noreply.github.com>
2024-02-22 16:34:47 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function direcionaGit(){
+    window.open("https://github.com/K4Z1NH0")
+};
+
+function direcionaFront(){
+    window.open("https://www.frontendmentor.io/profile/K4Z1NH0")
+};
+
+function direcionaLinkedin(){
+    window.open("https://www.linkedin.com/in/kayke-araujo-guimar%C3%A3es-a27a20221/")
+};
+
+function direcionaPortifolio(){
+    window.open("https://kaykearaujodev.netlify.app/");
+};
